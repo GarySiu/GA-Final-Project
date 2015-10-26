947850df4bef7675a816cb36b36ac48f876cdbe6
Gary Siu <Gary@untypeable.com>
2015-10-26 11:49:55 +0000

    Filter for different types of twitter search

--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
-var express = require('express');
-app = express();
+var express = require('express'),
+app = express(),
+bodyParser = require('body-parser'),
+jsonParser = bodyParser.json(),
+urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 port = process.env.PORT || 3000;
 
@@ -10,9 +13,23 @@ port = process.env.PORT || 3000;
 // MAGP_ACCESS_TOKEN_SECRET
 
 
-app.get('/', function(req, res){
-  res.send('Hello World!')
-})
+// app.get('/', function(req, res) {
+//   res.send('Hello World!')
+// })
+
+app.get('/search', urlencodedParser, function(req, res) {
+  // this gives us the option to return results from the web as well as twitter
+  if(req.query.type !== 'twitter') return res.sendStatus(400)
+
+  // different kinds of twitter search
+  if(req.query.q.charAt(0) === '@') {
+    res.send('User named: ' + req.query.q.slice(1))
+  } else if(req.query.q.charAt(0) === '#') {
+    res.send('Hashtag: ' + req.query.q.slice(1))
+  } else {
+    res.send('Generic search: ' + req.query.q)
+  }
+});
 
 console.log('Server up on port', port);
 app.listen(port);
