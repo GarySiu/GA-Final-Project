adbf6473f51089cc0988213fe8d7a02ab35439f2
Gary Siu <Gary@untypeable.com>
2015-10-26 18:46:06 +0000

    Magnets can be dragged while constrained to a div. There's an empty  build area where you can put them.

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
   $magnetList = $('#magnet-list');
+  $magnetContainer = $('#magnet-container');
 
-  $.get('http://localhost:3000/search?type=twitter&q=%40GA')
+  $.get('http://localhost:3000/search?type=twitter&q=%40velocikitty')
   .done(function(response) {
     var magnets = response;
-    $.each(magnets, function(index, magnet){
+    $.each(magnets, function(index, magnet) {
       $magnetList.append('<li>' + magnet + '</li>')
-    })
+    });
+
+    $(function() {
+      $( "li" ).draggable({ containment: $magnetContainer, stack: $magnetList });
+    });
+
+    $magnetContainer.append('<div id="build-area"></div>')
   })
 })
