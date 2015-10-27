a4be60bbd7682ad4c52b8871d85c527143e9b3a0
Gary Siu <Gary@untypeable.com>
2015-10-27 10:11:56 +0000

    Added searchbox

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,18 +1,32 @@
 $(document).ready(function() {
   $magnetList = $('#magnet-list');
   $magnetContainer = $('#magnet-container');
+  $searchBox = $('#search-box');
+  $searchButton = $('#search-button');
 
-  $.get('http://localhost:3000/search?type=twitter&q=%40velocikitty')
+  setListeners();
+  getMagnets();
+})
+
+function setListeners() {
+  $searchButton.on('click', function() {
+    event.preventDefault();
+    getMagnets();
+  })
+}
+
+function getMagnets() {
+  $.get('http://localhost:3000/search?type=twitter&q=' + encodeURIComponent($searchBox.val()))
   .done(function(response) {
+    $magnetList.empty();
     var magnets = response;
     $.each(magnets, function(index, magnet) {
       $magnetList.append('<li>' + magnet + '</li>')
     });
 
     $(function() {
-      $( "li" ).draggable({ cursor: 'move', containment: $magnetContainer, stack: '#magnet-list li'});
+      $( "li" ).draggable({ cursor: '-webkit-grabbing', containment: $magnetContainer, stack: '#magnet-list li'});
     });
-
-    $magnetContainer.append('<div id="build-area"></div>')
-  })
-})
+    // $magnetContainer.append('<div id="build-area"></div>');
+  });
+}
